d2c7d8e882b781f0659381687dfdec27e9247049
tayeful912 <kazitayefulislam12@gmail.com>
2022-05-13 00:07:33 +0600

    initial server setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000;
+
+// middle ware
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('server is runing')
+})
+
+app.listen(port, () => {
+    console.log(`server is running ar port number ${port}`)
+})
